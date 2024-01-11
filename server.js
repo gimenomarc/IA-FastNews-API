7dfb6f9a9f67570619f93b30d307d7bb945bec19
gimenomarc <gimenocervantesmarc@gmail.com>
2024-01-11 16:49:08 +0100

    scraping

--- server.js
+++ server.js
@@ -1,28 +1,61 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const Parser = require('rss-parser');
 const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 
-// Inicializar el parser de RSS
-const parser = new Parser();
-
 app.get('/noticias', async (req, res) => {
   try {
-    const rssFeedUrl = 'https://www.gadgets360.com/rss/apps/feeds'; // URL del feed RSS de Wired
+    // Inicializar Puppeteer
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-    // Obtener noticias del feed RSS
-    const feed = await parser.parseURL(rssFeedUrl);
-    const news = feed.items;
+    // Ir a la página de Xataka
+    console.log('Navigating to Xataka...');
+    await page.goto('https://www.xataka.com/', { waitUntil: 'domcontentloaded' });
 
-    // Enviar las noticias al frontend
-    res.json(news);
+    // Obtener enlaces a todos los artículos en la página principal directamente desde la estructura HTML
+    const articleLinks = await page.evaluate(() => {
+      const links = Array.from(document.querySelectorAll('.abstract-title a'));
+      return links.map(link => link.href);
+    });
+
+    // Cerrar el navegador, ya que ya hemos recopilado los enlaces
+    await browser.close();
+
+    console.log('Article links:', articleLinks);
+
+    if (!articleLinks.length) {
+      return res.status(404).json({ error: 'No se encontraron enlaces de artículos' });
+    }
+
+    // Obtener datos de cada artículo utilizando article-extractor
+    console.log('Fetching article data...');
+    const articlesData = await Promise.allSettled(articleLinks.map(async link => {
+      try {
+        const { extract } = await import('@extractus/article-extractor');
+        const article = await extract(link);
+        return article;
+      } catch (error) {
+        console.error(`Error al extraer datos de ${link}:`, error.message);
+        throw error;
+      }
+    }));
+
+    // Filtrar artículos nulos (donde hubo un error en la extracción)
+    const validArticlesData = articlesData
+      .filter(result => result.status === 'fulfilled' && result.value !== null)
+      .map(result => result.value);
+
+    console.log('Valid articles data:', validArticlesData);
+
+    // Enviar los datos de los artículos al frontend
+    res.json(validArticlesData);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error en el servidor' });
+    console.error('Error en la ruta /noticias:', error);
+    res.status(500).json({ error: error.message || 'Error en el servidor' });
   }
 });
 
